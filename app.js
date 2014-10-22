1e98f76a516f3211355897059c2c27b96199cf20
dustin-H <dustin.hoffner@gmail.com>
2014-10-22 16:57:56 +0200

    commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
-require('socket.io-client')
+require('socket.io-client');
 var app = express();
 
 app.get('/socket.io.js', function (req, res) {
-  res.sendFile(__dirname+'/node_modules/socket.io-client/socket.io.js');
+    res.sendFile(__dirname + '/node_modules/socket.io-client/socket.io.js');
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -12,13 +12,23 @@ var io = require('socket.io').listen(app.listen(3000));
 
 
 //JUST Some Test Code Following
-io.use(function(socket, next){
+io.use(function (socket, next) {
+    console.log(socket.request.url);
+    //console.log(socket.request._query);
     //console.log(socket.request);
-    if (socket.request.headers.cookie) return next();
-    next(new Error('Authentication error'));
+    if (socket.request._query.username && socket.request._query.password) {
+        console.log("Super");
+        next();
+    } else  {
+        next(new Error('Authentication error'));
+    }
+    //if (socket.request.headers.cookie) return next();
+    //next(new Error('Authentication error'));
 });
 
-var events = {};
+var events = {
+    "mytest": []
+};
 
 /*io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
@@ -27,23 +37,60 @@ var events = {};
   });
 });*/
 
+var counter = 0;
+
 io.sockets.on('connection', function (socket) {
-  socket.on('message', function (msg) {
-        socket.send("Hallo "+msg);
-  });
-  socket.on('eventbind', function (msg) {
-      if(!events[msg.event]){
-          events[msg.event] = [];
-      }
-      events[msg.event].push(socket);
-      //socket.send("Hallo "+msg);
-  });
-  socket.on('triggerevent', function (msg) {
-      if(!events[msg.event]){
-          events[msg.event] = [];
-      }
-      events[msg.event].push(socket);
-      //socket.send("Hallo "+msg);
-  });
-  socket.on('disconnect', function () { });
+    socket.send("HELLO SOMEONE");
+    if (!socket.dustin) {
+        socket.dustin = counter;
+        counter++;
+    }
+    console.log("NewUser: " + socket.dustin);
+
+    socket.on('message', function (msg) {
+        console.log("Hallo Super");
+        socket.send("Hallo " + msg);
+    });
+    socket.on('eventbind', function (msg) {
+        if (!events[msg.event]) {
+            events[msg.event] = [];
+        }
+        if (events[msg.event].indexOf(socket) < 0) {
+            events[msg.event].push(socket);
+            console.log("Add");
+        }
+        console.log(events.mytest.length);
+        //socket.send("Hallo "+msg);
+    });
+    socket.on('triggerevent', function (msg) {
+        if (events[msg.event]) {
+            for (var i in events[msg.event]) {
+                events[msg.event][i].send(msg.data);
+                console.log("Trigger");
+            }
+        }
+        console.log(events.mytest.length);
+        //events[msg.event].push(socket);
+        //socket.send("Hallo "+msg);
+    });
+    socket.on('ferret', function (name, fn) {
+
+
+        fn('woot');
+        socket.emit('ferret1', 'tobias', function (data) {
+            console.log("FERET = > "+data); // data will be 'woot'
+        });
+    });
+
+    socket.on('disconnect', function () {
+        console.log("LOG1");
+        for (var i in events) {
+            var j = events[i].indexOf(socket);
+            if (j >= 0) {
+                events[i].splice(j, 1);
+                console.log("removed");
+            }
+        }
+        console.log(events.mytest.length);
+    });
 });
--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
- var socket = io('http://localhost/');
+var token = "testtttkkkk";
+var socket = io('http://localhost:9001/?username=karl&password=test223');
  socket.on('connect', function () {
      socket.send('Karl');
 
@@ -6,4 +7,12 @@
          // my msg
          console.warn(msg);
      });
+
+     socket.on('ferret1', function (name, fn) {
+        fn('moinmoin');
+    });
+ });
+ socket.on('error', function(e){
+     console.error("error");
+     console.error(e);
  });
