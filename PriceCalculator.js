f152bd4ec7562296963ea5acc38e048ddca77e7d
Kkoile <Nils_Hirsekorn@online.de>
2014-10-29 19:51:30 +0100

    ready to merge

--- PriceCalculator.js
+++ PriceCalculator.js
@@ -12,12 +12,9 @@ var manuallySetPrices = [];
 
 m.calculatePrices = function () {
     drinkInterface.getAllDrinks(function error(err) {}, function cb(obj) {
-        var drinksWithPrices = [];
+        var drinksWithPrices = {};
         for (var i in obj) {
-            drinksWithPrices.push({
-                'id': obj[i]._id,
-                'price': calcPriceForDrink(obj[i])
-            });
+            drinksWithPrices[obj[i]._id] = {'id': obj[i]._id, 'price': calcPriceForDrink(obj[i])};
         }
         saveNewDrinkPricesToDatabase(drinksWithPrices);
     });
