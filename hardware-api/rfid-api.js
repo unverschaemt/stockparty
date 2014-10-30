93eb0fbd5b596dd218ed37cbd734df88b5e7fd7c
David Ehlen <dehlen@me.com>
2014-10-30 18:04:42 +0100

    ListenForScan implemented and tested, but needs more detailed testing to verifiy it will work correctly on every situation

--- hardware-api/rfid-api.js
+++ hardware-api/rfid-api.js
@@ -1,5 +1,7 @@
 var getmac = require('getmac');
 var monitor = require('usb-detection');
+var hid = require('hidstream');
+
 
 /* Constants for used RFID Reader model (8H10D-1) */
 var constants = {
@@ -8,14 +10,15 @@ var constants = {
 };
 
 var devices = [];
-
+var rfidReaders = [];
 var code = '';
+var allReaders = [];
 
 var rfidApi = {};
-/*
-var socket = {};
 
-socket.configdata = {
+//var socket = {};
+
+/*socket.configdata = {
     "global": {
         "configmode": true,
         "devicenameprefix": "device",
@@ -65,7 +68,7 @@ socket.configdata = {
 };
 
 socket.emit = function (a, b) {
-    console.log(a + ' => ' + b);
+    console.log(a + ' => ' + b.hid);
 }
 */
 
@@ -73,32 +76,88 @@ rfidApi.use = function (socket) {
     rfidApi.socket = socket;
 };
 
-
 /* Listens for a scan on all available RFID Readers */
-rfidApi.listenForScan = function () {
+rfidApi.listenForScan = function (arrIndex) {
+	console.log("Arr Index:");
+console.log(arrIndex);
+    var allDevices = hid.getDevices();
+    for (var j in allDevices) {
+        if (allDevices[j].productId === constants.PRODUCTID && allDevices[j].vendorId === constants.VENDORID) {
+			if(allReaders.length == 0) {
+				allReaders.push(allDevices[j]);
+			}
+			else {
+			for(var i in allReaders) {
+				if(allReaders[i].path !== allDevices[j].path) {
+            allReaders.push(allDevices[j]);
 
+					}
+			}
+		}
+        }
+    }
+       
+			console.log(allReaders);
+            var rfidReader = new hid.device(allReaders[arrIndex].path);
+			rfidReaders.push(rfidReader);
+			devices[arrIndex].path = ''+rfidReader.path;
+            rfidReader.on('data', function (dat) {
+                var c = dat.charCodes[0];
+                if (c != null) {
+                    if (c === '\n') { // /n at the end of every code
+                        console.log('data.received:');
+                        console.log(code);
+                        getmac.getMac(function (err, macAddress) {
+                            if (err) console.error(err);
+                            rfidApi.socket.emit('iddscan', {
+                                'hid': '' + macAddress +'-'+devices[arrIndex].serialNumber + '',
+                                'idk': '' + code + ''
+                            });
+                        });
+
+                        code = '';
+                    } else {
+                        code += c;
+                    }
+                }
+            });
+            rfidReader.on('error', function (err) {
+                console.warn(err);
+            });
+    
 }
+
+
 //TODO: Pfad holen und listener registrieren, Pfad mit node-hid möglich ?!
 monitor.on('add:' + constants.VENDORID + ':' + constants.PRODUCTID + '', function (scannedDevices, err) {
+	var arrayIndex = 0;
     if (devices.length == 0) {
+		console.log("erster reader");
         scannedDevices.serialNumber = '0';
         devices.push(scannedDevices);
 
     } else {
-        for (var i in devices) {
+        for (var i = 0;i<devices.length+1;i++) {
             if (devices[i] == null) {
                 scannedDevices.serialNumber = '' + i;
-                devices[i] = scannedDevices;				
+                devices[i] = scannedDevices;	
+				arrayIndex = i;			
                 break;
 
 
             } else {
+				if(i != 0) {
                 scannedDevices.serialNumber = '' + devices.length;
                 devices.push(scannedDevices);				
+				arrayIndex = devices.length -1;
+				}
 
             }
         }
     }
+
+		rfidApi.listenForScan(arrayIndex);
+
     getmac.getMac(function (err, macAddress) {
         if (err) console.error(err);
         if (rfidApi.socket.configdata.global.configmode == true) {
@@ -122,6 +181,13 @@ monitor.on('add:' + constants.VENDORID + ':' + constants.PRODUCTID + '', functio
 monitor.on('remove:' + constants.VENDORID + ':' + constants.PRODUCTID + '', function (scannedDevices, err) {
     var serNumber = getDevice(scannedDevices.locationId).serialNumber;
     nullDevice(scannedDevices.locationId);
+	
+	for(var i in rfidReaders) {
+		if(rfidReaders[i].path === scannedDevices.path) {
+			rfidReaders[i].close();	
+		}
+	}
+
     getmac.getMac(function (err, macAddress) {
         if (err) throw err;
         rfidApi.socket.emit('iddremove', {
