59d359e1ba218f6cd2bbd4a697f54199722264cf
Kkoile <Nils_Hirsekorn@online.de>
2014-11-13 09:10:53 +0100

    fixed bug that getGuest returns 'true' when there is no guest in database

--- database/GuestInterface.js
+++ database/GuestInterface.js
@@ -81,17 +81,27 @@ m.getGuest = function (idk, error, cb) {
         idk: idk
     }, function (err, guest) {
         if (err) {
+            var guest = {
+                'idk': idk,
+                'name': '',
+                'birthDate': 0
+            };
             m.addGuest(idk, '', 0, function (err) {
                 error(err)
             }, function (obj) {
-                cb(obj)
+                cb(guest);
             });
         } else {
             if (!guest) {
+                var guest = {
+                    'idk': idk,
+                    'name': '',
+                    'birthDate': 0
+                };
                 m.addGuest(idk, '', 0, function (err) {
                     error(err)
                 }, function (obj) {
-                    cb(obj)
+                    cb(guest);
                 });
             } else {
                 var temp = JSON.parse(JSON.stringify(guest));
