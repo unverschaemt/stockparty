558bb3f6c17d72d20937422e72821e68f0dc2ea4
Kkoile <Nils_Hirsekorn@online.de>
2015-04-22 14:00:12 +0200

    implemented Douglas Peucker algorithm to reduce datapoints of pricehistory

--- DrinkInterface.js
+++ DrinkInterface.js
@@ -30,6 +30,80 @@ m.getPriceEntry = function (error, callBack) {
     })
 };
 
+m.getPriceHistory = function (options ,error, callBack) {
+    priceHistoryInterface.getPriceHistory(error, function cb(history) {
+        drinkDatabaseInterface.getAllDrinks(error, function cb(drinks) {
+            var numberOfDrinks = 0;
+            for (var i in drinks) {
+              numberOfDrinks++;
+            }
+            var dataPoints = reduceDataPointsForAllDrinks(history, numberOfDrinks);
+            callBack(dataPoints);
+        });
+    })
+};
+
+reduceDataPointsForAllDrinks = function(history, numberOfDrinks){
+  var drinks = {};
+  var reducedDataPoints = {};
+  for(var i in history){
+    for(var drink in history[i].drinks){
+      if(!drinks[drink]){
+        drinks[drink] = [];
+      }
+      drinks[drink].push([history[i].time, history[i].drinks[drink].price]);
+    }
+  }
+  for(var drink in drinks){
+    reducedDataPoints[drink]=douglasPeucker(drinks[drink], 0.01);
+  }
+  return reducedDataPoints;
+};
+
+function douglasPeucker(points, epsilon) {
+        var i,
+            maxIndex = 0,
+            maxDistance = 0,
+            perpendicularDistance,
+            leftRecursiveResults, rightRecursiveResults,
+            filteredPoints;
+        // find the point with the maximum distance
+        for (i = 1; i < points.length - 1; i++) {
+            perpendicularDistance = findPerpendicularDistance(points[i], [points[1], points[points.length - 1]]);
+            if (perpendicularDistance > maxDistance) {
+                maxIndex = i;
+                maxDistance = perpendicularDistance;
+            }
+        }
+        // if max distance is greater than epsilon, recursively simplify
+        if (maxDistance >= epsilon) {
+            leftRecursiveResults = douglasPeucker(points.slice(1, maxIndex), epsilon);
+            rightRecursiveResults = douglasPeucker(points.slice(maxIndex), epsilon);
+            filteredPoints = leftRecursiveResults.concat(rightRecursiveResults);
+        } else {
+            filteredPoints = points;
+        }
+        return filteredPoints;
+};
+
+function findPerpendicularDistance(point, line) {
+        var pointX = point[0],
+            pointY = point[1],
+            lineStart = {
+                x: line[0][0],
+                y: line[0][1]
+            },
+            lineEnd = {
+                x: line[1][0],
+                y: line[1][1]
+            },
+            slope = (lineEnd.y - lineStart.y) / (lineEnd.x - lineStart.x),
+            intercept = lineStart.y - (slope * lineStart.x),
+            result;
+        result = Math.abs(slope * pointX - pointY + intercept) / Math.sqrt(Math.pow(slope, 2) + 1);
+        return result;
+    }
+
 m.buyDrinks = function (data, callBack) {
     var price = 0;
     var drinks = [];
